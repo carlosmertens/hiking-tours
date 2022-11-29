43120927b1f37e6a08f0e139de7041a96d400381
Carlos Mertens <mertenscarlos@yahoo.com>
2022-11-29 23:53:46 +0100

    Setup: colors, font, clear folders and files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { Lato } from '@next/font/google';
+import type { AppProps } from 'next/app';
+
+const lato = Lato({
+  weight: ['100', '300', '400', '700', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={lato.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
