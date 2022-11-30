6e2e535f347d30bc0700a0bfb9ca27b9043c454e
Carlos Mertens <mertenscarlos@yahoo.com>
2022-11-30 01:42:43 +0100

    Create: _documents file

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ export default function Home() {
           name='description'
           content='Book your next adventure. Hike in the most amazing places in Germany'
         />
-        <link rel='icon' href='/favicon.png' />
       </Head>
 
       <Header />
