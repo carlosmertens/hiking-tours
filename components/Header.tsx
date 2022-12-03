119ff98b92a06d50423ace7af945c028e7dd6632
Carlos Mertens <mertenscarlos@yahoo.com>
2022-12-03 01:27:45 +0100

    refactor: transform classes to bem

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 export const Header = () => {
   return (
     <header className='header'>
-      <div className='logo-box'>
+      <div className='header__logo-box'>
         <Image
-          className='logo'
+          className='header__logo'
           src='/logo-white.png'
           alt='Home logo'
           width={60}
@@ -13,13 +13,13 @@ export const Header = () => {
         />
       </div>
 
-      <div className='text-box'>
+      <div className='header__text-box'>
         <h1 className='heading-primary'>
-          <span className='heading-primary-main'>Outdoors</span>
-          <span className='heading-primary-sub'>is where life happens</span>
+          <span className='heading-primary--main'>Outdoors</span>
+          <span className='heading-primary--sub'>is where life happens</span>
         </h1>
 
-        <a className='btn btn-white btn-animated' href='#'>
+        <a className='btn btn--white btn--animated' href='#'>
           Discover our tours
         </a>
       </div>
