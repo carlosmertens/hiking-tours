fd4eaa3c501aa76daf1cfd325cc842320d8ea6b9
Carlos Mertens <mertenscarlos@yahoo.com>
2022-12-19 17:13:27 +0100

    feat: add and style text content on About component

--- components/About.tsx
+++ components/About.tsx
@@ -1,14 +1,38 @@
 export const About = () => {
   return (
     <section className='section-about'>
-      <div className='u-center-text u-margin-botton-8'>
+      <div className='u-center-text u-margin-botton-big'>
         <h2 className='heading-secondary'>
           Exciting tours for adventurous people
         </h2>
       </div>
 
       <div className='row'>
-        <div className='col-1-of-2'>text content</div>
+        <div className='col-1-of-2'>
+          <h3 className='heading-tertiary u-margin-button-small'>
+            You&apos;re going to fall in love with nature
+          </h3>
+          <p className='paragraph'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae iste
+            aspernatur provident suscipit dicta obcaecati molestias veniam
+            dolores ad voluptas expedita fugiat enim sapiente natus, ullam
+            dolorum animi, accusantium doloremque!
+          </p>
+
+          <h3 className='heading-tertiary u-margin-button-small'>
+            Live adventures like you never have before
+          </h3>
+          <p className='paragraph'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            accusamus perspiciatis eligendi beatae nesciunt. Illo, eaque eius,
+            qui esse laboriosam iste repudiandae, impedit ad beatae ea sit error
+            ipsa. Dolores?
+          </p>
+
+          <a href='#' className='btn-text'>
+            Learn more &rarr;
+          </a>
+        </div>
         <div className='col-1-of-2'>image composition</div>
       </div>
     </section>
