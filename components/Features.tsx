f95696fb931bc3008802b092acdc3d894c3f8095
Carlos Mertens <mertenscarlos@yahoo.com>
2023-01-06 23:50:49 +0100

    feat: create & style features section

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,59 @@
 export const Features = () => {
   return (
     <section className='section-features'>
-      <i className='icon-basic-world'></i>
+      <div className='row'>
+        <div className='col-1-of-4'>
+          <div className='feature-box'>
+            <i className='feature-box__icon icon-basic-world'></i>
+            <h3 className='heading-tertiary u-margin-button-small'>
+              Explore the world
+            </h3>
+            <p className='feature-box__text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus, iure enim?
+            </p>
+          </div>
+        </div>
+
+        <div className='col-1-of-4'>
+          <div className='feature-box'>
+            <i className='feature-box__icon icon-basic-compass'></i>
+            <h3 className='heading-tertiary u-margin-button-small'>
+              Meet nature
+            </h3>
+            <p className='feature-box__text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus, iure enim?
+            </p>
+          </div>
+        </div>
+
+        <div className='col-1-of-4'>
+          <div className='feature-box'>
+            <i className='feature-box__icon icon-basic-map'></i>
+            <h3 className='heading-tertiary u-margin-button-small'>
+              Find your way
+            </h3>
+            <p className='feature-box__text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus, iure enim?
+            </p>
+          </div>
+        </div>
+
+        <div className='col-1-of-4'>
+          <div className='feature-box'>
+            <i className='feature-box__icon icon-basic-heart'></i>
+            <h3 className='heading-tertiary u-margin-button-small'>
+              Life a healthier life
+            </h3>
+            <p className='feature-box__text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus, iure enim?
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
