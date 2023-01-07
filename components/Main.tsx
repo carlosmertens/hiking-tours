5f5fdda0884daf97b60ee2bc4f59a3607d4d1465
Carlos Mertens <mertenscarlos@yahoo.com>
2023-01-07 21:27:51 +0100

    feat: create Tour section component

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,11 +1,13 @@
 import { About } from './About';
 import { Features } from './Features';
+import { Tours } from './Tours';
 
 export const Main = () => {
   return (
     <main className='main'>
       <About />
       <Features />
+      <Tours />
     </main>
   );
 };
---
+++ components/Tours.tsx
@@ -0,0 +1,14 @@
+export const Tours = () => {
+  return (
+    <section className='section-tours'>
+      <div className='u-center-text u-margin-botton-big'>
+        <h2 className='heading-secondary'>Most popular tours</h2>
+      </div>
+      <div className='row'>
+        <div className='col-1-of-3'>col 1 of 3</div>
+        <div className='col-1-of-3'>col 2 of 3</div>
+        <div className='col-1-of-3'>col 3 of 3</div>
+      </div>
+    </section>
+  );
+};
